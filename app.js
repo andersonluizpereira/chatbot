fce024ff320494d1534fe6fa374e6a0a7b0ba13e
andersonluizpereira <andy2903.alp@gmail.com>
2018-08-17 01:40:57 -0300

    Arrumando opcoes do chat bot

--- app.js
+++ app.js
@@ -1,23 +1,56 @@
 const Telegram = require('telegram-node-bot')
+var request = require('request');
 const TelegramBaseController = Telegram.TelegramBaseController
 const TextCommand = Telegram.TextCommand
 const chatbot = new Telegram.Telegram('357005364:AAHkiEwtHHRXjF8rYvLsTvyo-R8LMC74lZY')
+class CasaDoCodigoController extends TelegramBaseController {
+    sendNovidades(scope) {
+        // request('http://newsapi.org/v2/top-headlines?country=br&category=business&apiKey=c1c39a0722cb44d4ad56266eea6cb9c4', function(error, response, body) {
+        //     // console.log('error:', error);
+        //     //console.log('statusCode:', response && response.statusCode);
+        //     // var parsedAlert = JSON.parse(body);
+        //     //console.log(parsedAlert)
+        //     //scope.sendMessage('As novidades são ' + JSON.parse(body));
+        // });
+        // request.end();
+        var options = {
+            hostname: 'http://newsapi.org',
+            port: 80,
+            path: '/v2/top-headlines?country=br&category=business&apiKey=c1c39a0722cb44d4ad56266eea6cb9c4',
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        };
 
-class CasaDoCodigoController extends TelegramBaseController{
-  
-  sendNovidades(scope){
-    scope.sendMessage('As novidades são nenhuma')
-  }
-  
-   get routes(){
-    return {
-      'sendNovidades': 'sendNovidades'
-      }
-   }  
+        var req = http.request(options, function(res) {
+            res.setEncoding('utf8');
+            res.on('data', function(data) {
+                console.log(data); // I can't parse it because, it's a string. why?
+            });
+        });
+        reqA.on('error', function(e) {
+            console.log('problem with request: ' + e.message);
+        });
+        reqA.end();
+    }
+    sendTemperaturas(scope) {
+        request('http://api.openweathermap.org/data/2.5/weather?q=Sao%20PAULO&APPID=22addef068318d53f14f6b2e5891fe37', function(error, response, body) {
+            console.log('error:', error);
+            console.log('statusCode:', response && response.statusCode);
+            var parsedWeather = JSON.parse(body);
+            scope.sendMessage('A temperatura atual em São Paulo é ' + parseInt((((parsedWeather['main']['temp'] - 32) / 1800) * 100)) + 'º');
+        });
+        request.end();
+    }
+
+    get routes() {
+        return {
+            'sendNovidades': 'sendNovidades',
+            'sendTemperaturas': 'sendTemperaturas'
+        }
+    }
 }
-
 chatbot.router
-        .when(
-         new TextCommand('/novidades','sendNovidades'), new CasaDoCodigoController()
-
-        )
+    .when(
+        new TextCommand('/temperaturas', 'sendTemperaturas'), new CasaDoCodigoController(),
+        new TextCommand('/novidades', 'sendNovidades'), new CasaDoCodigoController()
+    )
